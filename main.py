fa2e52c6f94da1303e5bc7beb5d3f5e1bda7f426
Korbin Vanette <116692587+KorbinVanetteDev@users.noreply.github.com>
2022-10-30 03:07:30 +0000

    Added more styling

--- main.py
+++ main.py
@@ -2,11 +2,33 @@ from flask import *
 import os
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = os.getenv('Key')
+app.config['SESSION_TYPE'] = 'filesystem'
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("index.html")
+
+@app.route('/css/')
+def css():
+  return send_file("static/style.css")
+
+
+@app.route('/bootstrap/')
+def boot():
+  return send_file("static/bootstrap.min.css")
+
+@app.route('/js/')
+def js():
+  return send_file("static/main.js")
+
+@app.errorhandler(404)
+def not_found(e):
+  return render_template("404.html")
 
 
 app.run(host='0.0.0.0', port=81)
