7b3d21689c29f774ba41f938aab46e0a11a0984c
Korbin Vanette <116692587+KorbinVanetteDev@users.noreply.github.com>
2022-11-05 01:16:29 +0000

    Updated header and added more content

--- main.py
+++ main.py
@@ -30,5 +30,13 @@ def js():
 def not_found(e):
   return render_template("404.html")
 
+@app.errorhandler(500)
+def overload(e):
+  return render_template("500.html")
+
+@app.route('/500')
+def test500():
+  return render_template("500.html")
+
 
 app.run(host='0.0.0.0', port=81)
